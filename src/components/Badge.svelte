5e9e75cebc40154cdf90b73790f24a39aecc9540
domingo2000 <dedwards@buk.cl>
2024-05-24 02:34:46 -0400

    feat: add badge component

---
+++ src/components/Badge.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let icon: string | undefined = undefined;
+	export let text = '';
+</script>
+
+<div class="badge">
+	{#if icon}
+		<i class="material-icons">{icon}</i>
+	{/if}
+	<div>{text}</div>
+</div>
+
+<style lang="scss">
+	.badge {
+		display: flex;
+		gap: var(--spacing-1);
+		align-items: center;
+		background-color: var(--color-secondary);
+		padding: var(--spacing-1) var(--spacing-2);
+		border-radius: var(--border-radius-3);
+		font-size: var(--font-size-sm);
+
+		i {
+			font-size: var(--font-size-med);
+		}
+	}
+</style>
