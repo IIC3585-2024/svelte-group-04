02767194f89ade1ca3b3185819ee31491c5df66c
domingo2000 <dedwards@buk.cl>
2024-05-24 02:34:57 -0400

    feat: add rating component

---
+++ src/components/Rating.svelte
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+	export let score: string = '';
+</script>
+
+<div class="rating">
+	<i class="material-icons rating__star">star</i>
+	<div>{score}</div>
+</div>
+
+<style scoped lang="scss">
+	.rating {
+		display: flex;
+		align-items: center;
+		gap: var(--spacing-1);
+
+		&__star {
+			color: #f1c40f;
+		}
+	}
+</style>
