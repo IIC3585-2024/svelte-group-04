1d51fa1df73a71b8a6f9524057fe25db36b5abbe
domingo2000 <dedwards@buk.cl>
2024-05-24 03:43:23 -0400

    feat: add loader to favorites view

--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -3,8 +3,9 @@
 	import { getTrails } from '../../services/trailsService';
 	import TrailList from '../../components/TrailList.svelte';
 	import type { Trail } from '../../types/Trail';
+	import Loader from '../../components/Loader.svelte';
 
-	let favoriteTrails: Trail[] = [];
+	let favoriteTrails: Trail[] | undefined = undefined;
 
 	favorites.subscribe(async (value) => {
 		if (!value) return;
@@ -19,5 +20,13 @@
 	});
 </script>
 
-<h1>Favorites</h1>
-<TrailList trails={favoriteTrails} />
+<div class="flex">
+	<h1>Favorites</h1>
+	{#if favoriteTrails === undefined}
+		<Loader />
+	{:else if favoriteTrails.length === 0}
+		<p>No favorite trails</p>
+	{:else}
+		<TrailList trails={favoriteTrails} />
+	{/if}
+</div>
